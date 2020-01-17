bb12c1d4fd5960269efd4d891bfdbf47b7d2583f
rizul <rizulgoyal23@gmail.com>
2020-01-17 16:31:40 -0500

    all api

--- API Demo/ViewController.swift
+++ API Demo/ViewController.swift
@@ -10,10 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var infoLabel: UILabel!
+    @IBOutlet var cityLabel: UITextField!
+    
+    
+    @IBAction func selectCity(_ sender: UIButton) {
+        
+        
+        let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(cityLabel.text!.replacingOccurrences(of: " ", with: "%20"))&appid=7a293fa3d1fda49318a6844883e9557f")!
+               let task = URLSession.shared.dataTask(with: url){
+                   (data,response, error) in
+                   if let error = error
+                   {
+                       print(error)
+                   }
+                   else
+                   {
+                       if let urlcontent = data{
+                           do {
+                               let jsonresult = try JSONSerialization.jsonObject(with: urlcontent, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary
+                               print(jsonresult!)
+                               print(jsonresult!["name"] as! String)
+                            let name = jsonresult!["name"] as! String
+                            
+                               if let weather = ((jsonresult!["weather"] as! NSArray)[0] as? NSDictionary)?["description"] as? String
+                               {
+                                   print(weather)
+                                DispatchQueue.main.async {
+                                    self.infoLabel.text = name + weather
+
+                                }
+                               }
+                               if let wind = (jsonresult!["wind"] as!  NSDictionary)["deg"] as? Int
+                                                      {
+                                                          print(wind)
+                                                      }
+
+                           }catch
+                           {
+                               print(error)
+                           }
+                       }
+                   }
+               }
+               
+               task.resume()
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+           }
+    
 
 
 }
